06979aec152079fb8385a01c2a3a5ae853367e91
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-05-08 23:32:40 -0300

    ?????????

--- app/shop/villageShop/index.js
+++ app/shop/villageShop/index.js
@@ -1,13 +1,25 @@
 import { itens } from "../../dataBase/listaItens.js";
-
+import initTable from "./villageShopTable.js";
 
 const shopBtn = document.getElementById('shop');
-const returnBtn = document.getElementById('village__shop__return__main__page__button');
+let shopBtnMode = false;
+const itensOnDisplay = [
+    itens.armour[1], 
+    itens.head[1], 
+    itens.gloves[0], 
+    itens.shoes[0], 
+    itens.sword[0]
+]
+
+console.log(itens.armour[1].hp)
 
 shopBtn.addEventListener('click', () => {
-    window.location.href = "../../../pages/villageShopTable.html"
-})
-
-returnBtn.addEventListener('click', () => {
-    window.location.href = "../../index.html"
+    const selectSection = document.querySelector('.village__shop__table')
+    if (shopBtnMode === false && !selectSection) {
+        initTable(itensOnDisplay)
+        return shopBtnMode = true
+    } else if (selectSection) {
+        selectSection.remove()
+        return shopBtnMode = false
+    }
 })
--- app/shop/villageShop/villageShopTable.js
+++ app/shop/villageShop/villageShopTable.js
@@ -1,10 +1,11 @@
-import { father } from "../../functions/createElement.js";
+import { father, createDiv } from "../../functions/createElement.js";
 import { createTable, createCaption, createTHead, createTr, createTh, createTBody, createTd, createTFoot } from "../../functions/createTable.js";
+import { Item } from "./vstItems.js";
 
 function initTable(shopItens){
 
     createTable(document.body, "village__shop__table")
-    createCaption(father(".village__shop__table"), "village__shop__table__caption", "Buy Itens")
+    createCaption(father(".village__shop__table"), "village__shop__table__caption", "Buy Items")
     createTHead(father(".village__shop__table"), "village__shop__table__head")
 
     createTr(father(".village__shop__table__head"), "village__shop__table__head__tr")
@@ -17,9 +18,10 @@ function initTable(shopItens){
     createTr(father(".village__shop__table__body"), "village__shop__table__body__tr")
 
     for(const item of shopItens){
-        createTh(father(".village__shop__table__body__tr"), "row", "village__shop__table__body__th", `${item.name}`)
-        createTd(father(".village__shop__table__body__tr"), "village__shop__table__body__td", `${item.id}`)
-        createTd(father(".village__shop__table__body__tr"), "village__shop__table__body__td", `${item.hp}`)
+        const div = document.querySelector(".village__shop__table__body__tr__div")
+        if(!div){
+            new Item(item).initBlock()
+        }
     }
 
     createTFoot(father(".village__shop__table"), "village__shop__table__foot")
