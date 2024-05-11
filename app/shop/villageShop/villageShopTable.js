fa77b86d0dd2bf4037a512ed11ef14e505d3b487
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-05-11 01:58:35 -0300

    ?

--- app/shop/villageShop/villageShopTable.js
+++ app/shop/villageShop/villageShopTable.js
@@ -2,6 +2,8 @@ import { father, createDiv } from "../../functions/createElement.js";
 import { createTable, createCaption, createTHead, createTr, createTh, createTBody, createTd, createTFoot } from "../../functions/createTable.js";
 import { Item } from "./vstItems.js";
 
+////////////////E SE EU CRIAR CLASSES PRA CADA COMPONENTE DO VILLAGE SHOP? DESSA FORMA, BASTA EU INSTANCIAR ELAS EM UM INDEX DE CADA SHOP, AS CLASSES SERÃO A BASE////////////////////////////////////////////////////
+
 function initTable(shopItens){
 
     createTable(document.body, "village__shop__table")
@@ -17,16 +19,19 @@ function initTable(shopItens){
     createTBody(father(".village__shop__table"), "village__shop__table__body")
     createTr(father(".village__shop__table__body"), "village__shop__table__body__tr")
 
+    const maxItemsPerRow = Math.max(...shopItens.map(item => item.length));
+
+    const itemWidthPercentage = 100 / maxItemsPerRow;
+
     for(const item of shopItens){
         new Item(item).initBlock()
-        
         
         const templateParentClassName = `.village__shop__table__body__tr__div__${item.id}` 
         const stringParentClassName = templateParentClassName.toString()
         const getDivClassName = document.querySelector(stringParentClassName)
         // getDivClassName.setAttribute("style", "display: flex")
         getDivClassName.style.display = "flex";
-        getDivClassName.style.justifyContent = "space-between";
+        getDivClassName.style.width = `${itemWidthPercentage}%`;
     }
 
     createTFoot(father(".village__shop__table"), "village__shop__table__foot")
